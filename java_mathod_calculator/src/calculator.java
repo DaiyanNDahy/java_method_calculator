6dc1325eac429ab2346cafd95cd26b1132695ca5
DaiyanNDahy <daiyanndahy@gmail.com>
2023-08-21 08:11:20 +0600

    data uploading

---
+++ java_mathod_calculator/src/calculator.java
@@ -0,0 +1,19 @@
+public class calculator{
+  
+    int performAddition(int a, int b){
+
+        return a+b;
+    }
+
+    int performSubtraction(int a, int b){
+
+        return a-b;
+    }
+
+    public static void main(String[] args){
+
+     
+
+    }
+    
+}
